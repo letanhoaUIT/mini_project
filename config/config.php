0a582e60fa516352e7fd3e2bc2eaaabc028de397
root <root@Hoa>
2024-07-04 15:38:36 +0700

    create structure and config.php file

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'php');
+define('DB_PASS', '12345');
+define('DB_NAME', 'mini_project');
+
+define('APPROOT', dirname(dirname(__FILE__)));
+define('URLROOT', 'http://localhost/mini_project');
+define('SITENAME', 'Mini Project');
+?>
