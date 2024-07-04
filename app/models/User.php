446a167c89b667e300eaba7e6f20c6ca156239c1
root <root@Hoa>
2024-07-04 16:20:30 +0700

    add files for models and views

---
+++ app/models/User.php
@@ -0,0 +1,14 @@
+<?php
+class User {
+    private $db;
+
+    public function __construct() {
+        $this->db = new Database;
+    }
+
+    public function getUsers() {
+        $this->db->query("SELECT * FROM users");
+        return $this->db->resultSet();
+    }
+}
+?>
